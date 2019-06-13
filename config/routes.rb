d75ed09889d1523eebf1be989aa1e054b7819aed
Matt Williams <matt.seongjun@gmail.com>
2019-06-13 14:54:16 -0400

    Working on views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    resources :recipes, only: [:index, :new]
-    resources :ingredients, only: [:index, :show, :new]
-    resources :users, only: [:show, :new, :delete]
+    resources :recipes #, only: [:index, :new]
+    resources :ingredients #, only: [:index, :show, :new]
+    resources :users #, only: [:show, :new, :delete]
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
